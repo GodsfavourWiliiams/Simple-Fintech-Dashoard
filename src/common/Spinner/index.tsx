3f278341b8df9fdd4e7d636bc6f979a4018d0d33
Godsfavour Williams <williamsgodsfavoursolomon@gmail.com>
2023-06-27 07:04:05 +0100

    Update index.tsx

--- src/common/Spinner/index.tsx
+++ src/common/Spinner/index.tsx
@@ -1,7 +1,7 @@
 const Spinner = () => {
   return (
     <div className="flex h-screen items-center justify-center bg-white relative">
-      <div className="h-16 w-16 animate-spin rounded-full border-4 border-solid border-primary border-t-transparent absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"></div>
+      <div className="h-16 w-16 animate-spin rounded-full border-4 border-solid border-primary border-t-transparent"></div>
     </div>
   );
 };
